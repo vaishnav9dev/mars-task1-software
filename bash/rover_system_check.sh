4b99c98c7d9c552df40c2b26998b023470ebae78
vaishnav9dev <vaishnav9dev@gmail.com>
2026-04-08 22:37:02 +0530

    Update rover_system_check.sh

--- bash/rover_system_check.sh
+++ bash/rover_system_check.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+echo "Checking system status..."
+
+battery=75
+
+if [ $battery -gt 20 ]; then
+    echo "Battery level sufficient: $battery%"
+else
+    echo "Warning: Low battery!"
+fi
+
+connection="active"
+
+if [ "$connection" == "active" ]; then
+    echo "Communication link established"
+else
+    echo "Communication failure"
+fi
