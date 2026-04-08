a1ced5aaefffbd3c34a510ae7f5d44c3d9ab37e0
vaishnav9dev <vaishnav9dev@gmail.com>
2026-04-08 22:36:28 +0530

    Update rover_commands.sh

--- bash/rover_commands.sh
+++ bash/rover_commands.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+echo "Creating mission directory..."
+mkdir -p rover_mission
+
+cd rover_mission
+
+echo "Creating mission log..."
+echo "Mission initialized successfully" > mission_log.txt
+
+echo "Listing files:"
+ls
